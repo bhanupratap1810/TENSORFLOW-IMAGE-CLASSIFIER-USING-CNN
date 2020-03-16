143e1b8a38d3bfab1d9061a7fd1ef8566bd2616d
bhanupratap1810 <52910511+bhanupratap1810@users.noreply.github.com>
2020-03-16 15:20:04 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -40,7 +40,7 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
                                    horizontal_flip = True)
-M 
+
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
